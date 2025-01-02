362f3e475ec6189643f5e26b4602055b8ee19b93
malamoo <rowanmalamoo@icloud.com>
2025-01-02 23:40:58 +1100

    Render an animated circle

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,18 +1,30 @@
 #import <Foundation/Foundation.h>
 
 #import "AppDelegate.h"
+#import "RenderView.h"
 
 @interface AppDelegate ()
-
 @property(strong) IBOutlet NSWindow *window;
+@property(strong) NSTimer *timer;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 / 60.0
+	                                              target:self
+	                                            selector:@selector(updateGraphics)
+	                                            userInfo:nil
+	                                             repeats:YES];
+}
+
+- (void)updateGraphics {
+	[self.window.contentView setNeedsDisplay:YES];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
+	[self.timer invalidate];
+	self.timer = nil;
 }
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
--- RenderView.m
+++ RenderView.m
@@ -1,4 +1,5 @@
 #import <AppKit/AppKit.h>
+#include <CoreGraphics/CoreGraphics.h>
 
 #import "RenderView.h"
 
@@ -7,9 +8,26 @@
 - (void)drawRect:(NSRect)dirtyRect {
 	[super drawRect:dirtyRect];
 
-	[[NSColor greenColor] setFill];
+	NSGraphicsContext *context = [NSGraphicsContext currentContext];
+	CGContextRef cgContext = [context CGContext];
 
-	NSRectFill(self.frame);
+	CGContextSetRGBFillColor(cgContext, 0.25, 0.5, 1, 1);
+	CGContextFillRect(cgContext, self.bounds);
+
+	static float angle = 0;
+	float radius = 50;
+	float centerX = NSMidX(self.bounds);
+	float centerY = NSMidY(self.bounds);
+
+	float circleX = centerX + cos(angle) * 100;
+	float circleY = centerY + sin(angle) * 100;
+
+	CGContextSetRGBFillColor(cgContext, 0.5, 0.7, 0.6, 1);
+	CGContextFillEllipseInRect(cgContext, CGRectMake(circleX - radius, circleY - radius, 2 * radius, 2 * radius));
+
+	angle += 0.05;
+	if (angle > 2 * M_PI)
+		angle -= 2 * M_PI;
 }
 
 @end
